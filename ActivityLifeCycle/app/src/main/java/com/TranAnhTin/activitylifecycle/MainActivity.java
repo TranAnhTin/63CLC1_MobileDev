27f4c1c52fe472c1b8d0265d8df919c171cbaa65
TranAnhTin <144192851+TranAnhTin@users.noreply.github.com>
2023-09-25 18:29:03 +0700

    Ex6

--- ActivityLifeCycle/app/src/main/java/com/TranAnhTin/activitylifecycle/MainActivity.java
+++ ActivityLifeCycle/app/src/main/java/com/TranAnhTin/activitylifecycle/MainActivity.java
@@ -3,17 +3,8 @@ package com.TranAnhTin.activitylifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
 }
